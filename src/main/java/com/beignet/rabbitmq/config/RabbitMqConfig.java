9df1bea10f182ad3cf7245eea2fdb5e365ce4b00
Beigent <1311724164@qq.com>
2020-09-15 15:48:00 +0800

    nothing

---
+++ src/main/java/com/beignet/rabbitmq/config/RabbitMqConfig.java
@@ -0,0 +1,9 @@
+package com.beignet.rabbitmq.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+}
